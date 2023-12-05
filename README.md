5fef0eb9c10ed2ad3e3221676b17bfacc75a594f
Reperak <reperak5fba@protonmail.com>
2023-12-04 19:35:55 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# alexandria
+Shoddy tool for automatically organizing a FLAC collection into a directory structure based on metadata
+
+# Usage
+`cd` into the directory of your FLAC collection and run `alexandria`. Alexandria will show you a preview of how it will reorganize your FLACs in the format `src -> dst`. Enter `y` if you approve of the changes, and Alexandria will perform the reorganization.
+
+Note that Alexandria is not tested for metadataless FLACs. Don't yell at me if it implodes.
+
+# Contributing
+Don't open issues asking me to add features. Do it yourself and submit a pull request. By doing so you agree to license your contributions under [Alexandria's license](#license).
+
+# License
+Alexandria is licensed under the GNU General Public License version 3 or, at your option, any later version. See LICENSE for details.
---
+++ src/main.rs
@@ -0,0 +1,67 @@
+// Copyright (c) 2023 Reperak
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use std::collections::HashMap;
+use std::env::current_dir;
+use std::fs::create_dir_all;
+use std::fs::rename;
+use std::io::stdin;
+use std::path::PathBuf;
+
+use claxon::FlacReader;
+use walkdir::WalkDir;
+
+fn main()
+{
+    let mut mappings: HashMap<PathBuf, PathBuf> = HashMap::new();
+
+    for file in WalkDir::new(current_dir().unwrap())
+        .into_iter()
+        .filter_map(|res| res.ok())
+        .filter(|dir| dir.path().extension().unwrap_or_default() == "flac")
+        .map(|dir| dir.path().to_owned())
+    {
+        let reader = FlacReader::open(&file).unwrap();
+
+        let mut new_path = current_dir().unwrap();
+        new_path.push(reader.get_tag("ARTIST").next().unwrap_or_default());
+        new_path.push(reader.get_tag("ALBUM").next().unwrap_or_default());
+        new_path.push(reader.get_tag("TITLE").next().unwrap_or_default());
+        new_path.set_extension("flac");
+
+        mappings.insert(file, new_path);
+    }
+
+    for (old_path, new_path) in mappings.iter() {
+        println!(
+            "{} -> {}",
+            old_path.strip_prefix(current_dir().unwrap()).unwrap().display(),
+            new_path.strip_prefix(current_dir().unwrap()).unwrap().display()
+        );
+    }
+
+    let mut input = String::new();
+    println!("Confirm? (y/N)");
+    stdin().read_line(&mut input).unwrap();
+
+    match input.as_str() {
+        "y" | "Y" =>
+            for (old_path, new_path) in mappings.iter() {
+                create_dir_all(new_path.parent().unwrap()).unwrap();
+                rename(old_path, new_path).unwrap();
+            },
+        _ => println!("Aborted"),
+    }
+}
